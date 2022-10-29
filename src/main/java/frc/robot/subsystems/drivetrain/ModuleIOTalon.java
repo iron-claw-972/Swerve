c42984059177ebce14d8b6dbd3959927451f99f3
AnthonyFurman <71467649+AnthonyFurman@users.noreply.github.com>
2022-10-29 13:26:09 -0700

    fix import

--- src/main/java/frc/robot/subsystems/drivetrain/ModuleIOTalon.java
+++ src/main/java/frc/robot/subsystems/drivetrain/ModuleIOTalon.java
@@ -10,6 +10,7 @@ import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
 import frc.robot.constants.Constants;
 
 public class ModuleIOTalon implements ModuleIO {
