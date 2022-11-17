06609b7441423ccc36fd036371dc237cde4c4f2a
AnthonyFurman <71467649+AnthonyFurman@users.noreply.github.com>
2022-11-17 15:54:25 -0800

    move decreased velocity to drive

--- src/main/java/frc/robot/commands/DefaultDriveCommand.java
+++ src/main/java/frc/robot/commands/DefaultDriveCommand.java
@@ -29,13 +29,13 @@ public class DefaultDriveCommand extends CommandBase {
         // Get the x speed. We are inverting this because Xbox controllers return
         // negative values when we push forward.
         final var xSpeed = MathUtil.applyDeadband(Driver.getRawLeftY(), Constants.oi.kDeadband)
-                * Constants.drive.kMaxSpeed;
+                * Constants.drive.kMaxSpeed * 0.5;
     
         // Get the y speed or sideways/strafe speed. We are inverting this because
         // we want a positive value when we pull to the left. Xbox controllers
         // return positive values when you pull to the right by default.
         final var ySpeed = MathUtil.applyDeadband(Driver.getRawLeftX(), Constants.oi.kDeadband)
-                * Constants.drive.kMaxSpeed;
+                * Constants.drive.kMaxSpeed * 0.5;
     
         // Get the rate of angular rotation. We are inverting this because we want a
         // positive value when we pull to the left (remember, CCW is positive in
